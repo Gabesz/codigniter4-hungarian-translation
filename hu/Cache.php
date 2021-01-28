bb6fbf1a6263dd9a834c8c3d16eb7893a4d34606
Gabesz Takacs <tgabesz@gmail.com>
2021-01-28 19:55:11 +0100

    Cache is ready to use

---
+++ hu/Cache.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Cache language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'unableToWrite'   => 'A gyorsítótár nem írható a(z) {0} tárolóba.',
+   'invalidHandlers' => 'A gyorsítótár konfiguráció a(z) $validHandlers tömbnek kell lennie.',
+   'noBackup'        => 'A gyorsítótárnak kötelező beállítani egy kezelőt(handler) és biztonságimentés kezelőt(backupHandler).',
+   'handlerNotFound' => 'A gyorsítótárnak szabálytalan kezelő(handler) vagy biztonságimentés kezelő(backupHandler) van beállítva.',
+];
