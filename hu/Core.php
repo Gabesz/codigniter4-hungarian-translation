a12ff5a839ded76456c12f916b65c56379c7e50c
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 09:29:12 +0100

    Core is ready to use

---
+++ hu/Core.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Core language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'copyError'                    => 'Hiba történt a(z) ({0}) cseréje közben. Kérjük, ellenőrizze, hogy a könyvtár írható-e.',
+   'enabledZlibOutputCompression' => 'A zlib.output_compression ini direktíva be van kapcsolva. Ez nem fog jól működni a kimeneti puffereknél.',
+   'invalidFile'                  => 'Érvénytelen fájl: {0}',
+   'missingExtension'             => '{0} kiterjesztés nincs betöltve.',
+   'noHandlers'                   => '{0} legalább egy kezelőt meg kell adni.',
+];
