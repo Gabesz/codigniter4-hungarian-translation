e13ac3dd5dcb119d07d9ad22bdcfa82229a91447
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 11:45:01 +0100

    Encryption is ready to use

---
+++ hu/Encryption.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Encryption language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noDriverRequested'    => 'Nem állított be driver-t.',
+   'noHandlerAvailable'   => 'Nem található elérhető {0} titkosítási kezelő.',
+   'unKnownHandler'       => '"{0}" nem konfigurálható.',
+   'starterKeyNeeded'     => 'A titkosítónak indító kulcsra van szüksége.',
+   'authenticationFailed' => 'Dekódolás: a hitelesítés sikertelen.',
+   'encryptionFailed'     => 'A titkosítás sikertelen.',
+];
