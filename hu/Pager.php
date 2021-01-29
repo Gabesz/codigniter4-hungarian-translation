59c064b660385aafd9b09a6e4e991900e186ea7c
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 12:04:05 +0100

    Pager is ready to use

---
+++ hu/Pager.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Pager language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'pageNavigation'         => 'Oldal navigáció',
+   'first'                  => 'Első',
+   'previous'               => 'Előző',
+   'next'                   => 'Következő',
+   'last'                   => 'Utolsó',
+   'older'                  => 'Régi',
+   'newer'                  => 'Új',
+   'invalidTemplate'        => '{0} egy érvénytelen lapozó sablon.',
+   'invalidPaginationGroup' => '{0} egy érvénytelen lapozó csoport.',
+];
