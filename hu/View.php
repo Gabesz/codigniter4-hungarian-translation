89a876ce6823db6e2aad6c75c388f770d8b591dd
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 12:24:15 +0100

    View is ready to use

---
+++ hu/View.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * View language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidCellMethod'     => '{osztály}::{metódus} érvénytelen metódus.',
+   'missingCellParameters' => '{osztály}::{metódus} nem tartalmaz paramétert.',
+   'invalidCellParameter'  => '{0} érvénytelen paraméter megnevezés.',
+   'noCellClass'           => 'Nem adorr meg cella osztály (view cell).',
+   'invalidCellClass'      => '{0} cella osztály (view cell) nem elérhető.',
+   'tagSyntaxError'        => 'Szintaxishiba van az értelmező címkékben: {0}',
+];
