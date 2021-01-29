741bac7f648401eb8dcbfb5c2590845a10b78318
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 12:08:39 +0100

    Router is ready to use

---
+++ hu/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Router language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidParameter'    => 'Egy paraméter nem egyezik a várt típussal.',
+   'missingDefaultRoute' => 'Nem sikerült meghatározni, hogy mit kell megjeleníteni. Alapértelmezett útvonal nincs megadva az \'routing\' fájlban.',
+];
