df4cc5e34f123ae4d3c1409809663d3e1688fcb1
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 11:53:20 +0100

    Files is ready to use

---
+++ hu/Files.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Files language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'fileNotFound' => 'A fájl nem elérhető: {0}',
+   'cannotMove'   => 'A fájl nem másolható {0} a(z) {1} ({2})',
+];
