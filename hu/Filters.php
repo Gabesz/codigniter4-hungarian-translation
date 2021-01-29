367ad264ac5d95afe41fa79796491c301274c31e
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 11:55:48 +0100

    Filters is ready to use

---
+++ hu/Filters.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Filters language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noFilter'           => '{0} szűrő nem rendelkezik érvényes alias névvel.',
+   'incorrectInterface' => '{0} osztálynak meg kell valósítani a CodeIgniter\Filters\FilterInterface interfészt.',
+];
