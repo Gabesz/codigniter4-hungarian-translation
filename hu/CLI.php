56860a4793171e63df8789a9695fe214e1c59d94
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 09:24:59 +0100

    CLI is ready to use

---
+++ hu/CLI.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * CLI language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'commandNotFound' => '"{0}" parancs nem található.',
+   'helpUsage'       => 'Használat:',
+   'helpDescription' => 'Leírás:',
+   'helpOptions'     => 'Opciók:',
+   'helpArguments'   => 'Argumentumok:',
+   'invalidColor'    => 'Érvénytelen {0} szín: {1}.',
+];
