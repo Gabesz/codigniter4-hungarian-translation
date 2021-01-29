6e9d4dda571c13fb91734574258fe3e8fd62e1b2
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 11:48:56 +0100

    Entity is ready to use

---
+++ hu/Entity.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * CLI language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'tryingToAccessNonExistentProperty' => '{0} megpróbál hozzáférni egy nem létező tulajdonsághoz ({1}).',
+];
