7024ba15608d307d8d03181a9d76f9c33e344a53
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 12:36:34 +0100

    Time is ready to use

---
+++ hu/Time.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Time language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidMonth'   => 'A hónapoknak 1 és 12 között kell lenniük. Megadva: {0}',
+   'invalidDay'     => 'A napoknak 1 és 31 között kell lenniük. Megadva: {0}',
+   'invalidOverDay' => 'A napoknak 1 és {0} között kell lenniük. Megadva: {1}',
+   'invalidHours'   => 'Az óráknak 0 és 23 között kell lenniük. Megadva: {0}',
+   'invalidMinutes' => 'A perceknek 0 és 59 között kell lenniük. Megadva: {0}',
+   'invalidSeconds' => 'A másodperceknek 0 és 59 között kell lenniük. Megadva: {0}',
+   'years'          => '{0, plural, =1{# év} other{# years}}',
+   'months'         => '{0, plural, =1{# hónap} other{# hónap}}',
+   'weeks'          => '{0, plural, =1{# hét} other{# hét}}',
+   'days'           => '{0, plural, =1{# nap} other{# nap}}',
+   'hours'          => '{0, plural, =1{# óra} other{# óra}}',
+   'minutes'        => '{0, plural, =1{# perc} other{# perc}}',
+   'seconds'        => '{0, plural, =1{# másodperc} other{# másodperc}}',
+   'ago'            => '{0} előtt',
+   'inFuture'       => '{0} után',
+   'yesterday'      => 'Tegnap',
+   'tomorrow'       => 'Holnap',
+   'now'            => 'Épp most',
+];
