e72d3fedda997227d59ecf9dd2a8e8cf6be115f4
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 12:27:46 +0100

    Seed is ready to use

---
+++ hu/Seed.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Seed language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+    // Seed command
+    'nameFile'   => 'Nevezze el a \'seeder\' fájlt',
+    'writeError' => 'Hiba a fájl {0} létrehozása során, ellenőrizze a könyvtár jogosúltságait. Jelenleg nem írható.',
+];
