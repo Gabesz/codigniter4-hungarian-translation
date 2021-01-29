305e62259c4ad9506df6aed821e4aa28ba8b68be
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 09:40:59 +0100

    Database is ready to use

---
+++ hu/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Database language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidEvent'                     => '{0} érvénytelen model esemény hívás.',
+   'invalidArgument'                  => 'Meg kell adnia egy érvényes {0}.',
+   'invalidAllowedFields'             => 'Meg kell adnia az engedélyezett mezőket a modelnek: {0}',
+   'emptyDataset'                     => 'Nincs adat {0}.',
+   'failGetFieldData'                 => 'Nem sikerült a mező adatait lekérdezni az adatbázisból.',
+   'failGetIndexData'                 => 'Nem sikerült az indexet lekérdezni az adatbázisból.',
+   'failGetForeignKeyData'            => 'Nem sikerült az idegen kulcsot lekérdezni az adatbázisból.',
+   'parseStringFail'                  => 'A kulcs karakterláncának elemzése sikertelen.',
+   'featureUnavailable'               => 'Ez a szolgáltatás nem érhető el a használt adatbázisban.',
+   'tableNotFound'                    => '`{0}` tábla nem található a jelenleg használt adatbázisban.',
+   'noPrimaryKey'                     => '`{0}` model nem definiálja az elsődleges kulcsot.',
+   'noDateFormat'                     => '`{0}` modelnek nincs érvényes dátumformátuma.',
+   'fieldNotExists'                   => '`{0}` mező nem található.',
+   'forEmptyInputGiven'               => 'A(z) `{0}` mezőre üres utasítást adott meg',
+   'forFindColumnHaveMultipleColumns' => 'Csak egyetlen mező engedélyezett a mező névben.',
+];
