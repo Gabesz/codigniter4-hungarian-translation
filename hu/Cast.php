67f863db70d70e7cec8f5938dae3734d0ba88c94
Gabesz Takacs <tgabesz@gmail.com>
2021-01-29 09:22:06 +0100

    Cast is ready to use

---
+++ hu/Cast.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Cast language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'jsonErrorDepth'         => 'Túllépte az elérhető memóriát',
+   'jsonErrorStateMismatch' => 'Alulcsordulás vagy a módok nem egyeznek',
+   'jsonErrorCtrlChar'      => 'Váratlan vezérlő karakter található',
+   'jsonErrorSyntax'        => 'Szintaxis hiba, hibás JSON formátum',
+   'jsonErrorUtf8'          => 'Rosszul formázott UTF-8 karakterek, esetleg helytelenül kódolva',
+   'jsonErrorUnknown'       => 'Ismeretlen hiba',
+];
